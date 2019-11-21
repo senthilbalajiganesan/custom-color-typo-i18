9afeb1766235efd99a7fdad2c171d0591823191e
senthilbalajiganesan <senthilbalaji.ganesan>
2019-11-22 00:53:13 +0530

    initial set up

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Card.scss";
+
+export default (props: any) => {
+  return <div>{props.children}</div>;
+};
---
+++ src/screens/Main.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default (props: any) => {
+  return <div>App Screen, this should aligned center</div>;
+};
