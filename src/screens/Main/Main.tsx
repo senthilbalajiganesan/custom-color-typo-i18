7b2b6f1d4f2254379b7244536d02119f2796135c
senthilbalajiganesan <senthilbalaji.ganesan>
2019-11-22 17:44:03 +0530

    changed languages

--- src/screens/Main/Main.tsx
+++ src/screens/Main/Main.tsx
@@ -29,7 +29,7 @@ export default (props: any) => {
           list={[
             { name: "English", value: "en" },
             { name: "Tamil", value: "tamil" },
-            { name: "English (us)", value: "en" }
+            { name: "English (UK)", value: "en" }
           ]}
           onChange={(event: any) => i18n.changeLanguage(event.target.value)}
         />
